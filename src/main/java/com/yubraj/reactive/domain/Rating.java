add6c52fd407b23bb96601fc8249ac398bf134b8
Pokharel <s2151621@7-11.com>
2023-04-05 10:57:03 -0500

    initial rating implementation

--- src/main/java/com/yubraj/reactive/domain/Rating.java
+++ src/main/java/com/yubraj/reactive/domain/Rating.java
@@ -1,4 +1,23 @@
 package com.yubraj.reactive.domain;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("rating")
+@Data
 public class Rating {
+  @Id private String id;
+
+  @NotBlank private String userId;
+
+  @NotBlank
+  private String movieId;
+
+  @Min(1)
+  @Max(10)
+  private int rating;
 }
--- src/main/java/com/yubraj/reactive/repo/RatingRepository.java
+++ src/main/java/com/yubraj/reactive/repo/RatingRepository.java
@@ -1,10 +1,11 @@
 package com.yubraj.reactive.repo;
 
-import com.yubraj.reactive.domain.Users;
+import com.yubraj.reactive.domain.Rating;import com.yubraj.reactive.domain.Users;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;import reactor.core.publisher.Mono;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
-public interface RatingRepository extends ReactiveMongoRepository<Users, String> {
-  Mono<Users> findByEmail(String email);
+public interface RatingRepository extends ReactiveMongoRepository<Rating, String> {
+  Mono<Rating> findByUserId(String userId);
 }
